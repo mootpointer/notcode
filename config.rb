171cc4876a3a4a4e6985fd9ecd3342f7065b0646
Andrew Harvey <andrew@mootpointer.com>
2015-03-26 10:47:43 +1100

    Add favicon

--- config.rb
+++ config.rb
@@ -71,6 +71,17 @@ set :images_dir, 'images'
 
 page '/podcast.rss', :layout => false, :directory_index => false
 
+activate :favicon_maker, :icons => {
+  "_favicon.png" => [
+    { icon: "apple-touch-icon-152x152-precomposed.png" },
+    { icon: "apple-touch-icon-114x114-precomposed.png" },
+    { icon: "apple-touch-icon-72x72-precomposed.png" },
+  ],
+  "_favicon_lores.png" => [
+    { icon: "favicon.png", size: "16x16" },
+    { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
+  ]
+}
 activate :sync do |sync|
   sync.fog_provider = "AWS"
   sync.fog_directory = "notco.de"
